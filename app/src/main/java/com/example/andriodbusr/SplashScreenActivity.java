27e568f2c6e0c92d4000c0f8a4dfd9bfce526a44
sohamshindework <86647189+sohamshindework@users.noreply.github.com>
2021-07-21 11:32:24 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/andriodbusr/SplashScreenActivity.java
@@ -0,0 +1,110 @@
+package com.example.andriodbusr;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    private static final int LOGIN_REQUEST_CODE = 2001;
+    private List<AuthUI.IdpConfig>providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        delaySplashScreen();
+    }
+
+    @Override
+    protected void onStop() {
+        if(firebaseAuth!=null && listener!=null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        init();
+
+    }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser user=myFirebaseAuth.getCurrentUser();
+            if (user!=null)
+                Toast.makeText(this, "Welcome:"+user.getUid(), Toast.LENGTH_SHORT).show();
+            else
+                showLoginLayout();
+
+        };
+
+    }
+
+    private void showLoginLayout() {
+        AuthMethodPickerLayout authMethodPickerLayout=new AuthMethodPickerLayout
+                .Builder(R.layout.layout_sign_in)
+                .setPhoneButtonId(R.id.btn_phone_sign_in)
+                .setGoogleButtonId(R.id.btn_google_sign_in)
+                .build();
+        startActivityForResult(AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAuthMethodPickerLayout(authMethodPickerLayout)
+        .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+        .setAvailableProviders(providers)
+        .build(),LOGIN_REQUEST_CODE);
+    }
+
+    private void delaySplashScreen() {
+        Completable.timer(5, TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(() ->
+
+                        firebaseAuth.addAuthStateListener(listener)
+
+                        );
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==LOGIN_REQUEST_CODE) {
+            IdpResponse response=IdpResponse.fromResultIntent(data);
+
+
+            if (requestCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                Toast.makeText(this, "[ERROR]: "+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
